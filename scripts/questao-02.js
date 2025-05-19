2b19bf57fd913a745dc67c2a92210bb122b802c8
Rebeca <rebeca.escobar@aluno.senai.br>
2025-05-19 09:42:21 -0300

    feat: finaliza resolução da questão 02

--- scripts/questao-02.js
+++ scripts/questao-02.js
@@ -1,1 +1,18 @@
-const Numeros = [12, 5, 8, 21, 16, 7, 30, 45, 13, 27];
+const numeros = [12, 5, 8, 21, 16, 7, 30, 45, 13, 27];
+let numerosPares = 0;
+let maioresQue20 = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 == 0) {
+    numerosPares++;
+  } else {
+    numerosImpares++;
+  }
+}
+if (numeros[i] > 20) {
+  maioresQue20++;
+}
+
+console.log(`A quantidade de números pares é ${numerosPares}`);
+console.log(`A quantidade de números ímpares é ${numerosImpares}`);
+console.log(`A quantidade de números maiores que 20 é ${maioresQue20}`);
